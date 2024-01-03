b850eb30300e726f106c21167c673124f3f47f8e
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2024-01-03 15:23:44 -0300

    Aula 006 - Node JS

--- aula006-revisao/index.js
+++ aula006-revisao/index.js
@@ -1,14 +1,14 @@
 // PREPARACAO DO EXPRESS ...
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // REQUISICOES ...
-app.get('/', (req, res) => {
-    res.json = {mensagem: 'Hello, World!'};
+app.get("/", (req, res) => {
+  res.json({ mensagem: "Hello, World!" });
 });
 
 // MONITORAMENTO DAS REQUISICOES ...
 app.listen(8080, () => {
-    let data = new Date();
-    console.log('Servidor node iniciado em: ' + data);
+  let data = new Date();
+  console.log("Servidor node iniciado em: " + data);
 });
